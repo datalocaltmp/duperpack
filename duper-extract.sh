f03b6944f420d683322fcbcdb651d91bb635910b
dev <dev@devs-Mac-mini.local>
2023-06-11 12:55:12 -0400

    Update with frida:

--- duper-extract.sh
+++ duper-extract.sh
@@ -1,8 +1,12 @@
 
 
-/Users/runner/Library/Android/sdk/platform-tools/adb devices
+wget https://github.com/frida/frida/releases/download/16.0.19/frida-server-16.0.19-android-x86_64.xz
+unxz ./frida-server-16.0.19-android-x86_64.xz
+
 adb root
-adb push ./frida-server-android /data/local/tmp
-adb shell chmod 777 /data/local/tmp/frida-server-android
-adb shell /data/local/tmp/frida-server-android &
+
+adb push ./frida-server-16.0.19-android-x86_64 /data/local/tmp
+adb shell chmod 777 /data/local/tmp/frida-server-16.0.19-android-x86_64
+adb shell /data/local/tmp/frida-server-16.0.19-android-x86_64 &
+frida-ps -U
 
