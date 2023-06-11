19baff81b7ed5f69d90649ab2d45fc0628301d8a
dev <dev@devs-Mac-mini.local>
2023-06-11 12:33:50 -0400

    Adding Android portion

--- duper-extract.sh
+++ duper-extract.sh
@@ -1,3 +1,8 @@
-echo "Hello world!"
+
+
 /Users/runner/Library/Android/sdk/platform-tools/adb devices
-adb devices
+adb root
+adb push ./frida-server-android /data/local/tmp
+adb shell chmod 777 /data/local/tmp/frida-server-android
+adb shell /data/local/tmp/frida-server-android &
+
