0b9dd16c2eed2ef4b5c1b89d4c11246d5904f935
dev <dev@devs-Mac-mini.local>
2023-06-18 20:32:04 -0400

    Debugging

--- duper-extract.sh
+++ duper-extract.sh
@@ -11,7 +11,7 @@ export PATH=$PATH:/Users/runner/Library/Android/sdk/build-tools/29.0.0/
 
 # Replace libs.spk.zst with x86 libs.spk.zst 
 apktool d -rs WhatsApp.apk
-mv WhatsApp/assets/compressed/x86/libs.spk.zst WhatsApp/assets/compressed/arm64-v8a/.
+mv WhatsApp/assets/compressed/x86_64/libs.spk.zst WhatsApp/assets/compressed/arm64-v8a/.
 apktool b WhatsApp
 java -jar ./uber-apk-signer-1.3.0.jar -a ./WhatsApp/dist/WhatsApp.apk && ls ./WhatsApp/dist/ && /Users/runner/Library/Android/sdk/platform-tools/adb install WhatsApp/dist/WhatsApp-aligned-debugSigned.apk
 rm -rf ./WhatsApp
@@ -33,7 +33,7 @@ rm -rf ./WhatsApp
 
 # Replace libs.spk.zst with armeabi-v7a libs.spk.zst 
 apktool d -rs WhatsApp.apk
-mv WhatsApp/assets/compressed/armeabi-v7a/libs.spk.zst WhatsApp/assets/compressed/arm64-v8a/.
+mv WhatsApp/assets/compressed/x86_64/libs.spk.zst WhatsApp/assets/compressed/arm64-v8a/.
 apktool b WhatsApp
 java -jar ./uber-apk-signer-1.3.0.jar -a ./WhatsApp/dist/WhatsApp.apk && ls ./WhatsApp/dist/ && /Users/runner/Library/Android/sdk/platform-tools/adb install WhatsApp/dist/WhatsApp-aligned-debugSigned.apk
 rm -rf ./WhatsApp
